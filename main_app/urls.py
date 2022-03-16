19f9f062de46032e298f0049a0b8345b5187748b
suyog2398 <suyogkapdekar.23@gmail.com>
2022-03-16 10:07:27 +0530

    initial commit

---
+++ main_app/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+urlpatterns=[
+    path('',views.home,name='home'),
+    path('quiz/',views.quiz,name='quiz'),
+    path('result/',views.result,name='result'),
+    
+   
+   
+    
+]
